20cf0457e878b9db4b9b0cf5c1effc71932f916f
Al3xGonzales <90079263+Al3xGonzales@users.noreply.github.com>
2024-10-09 13:22:39 -0700

    Update cryptography.py
    added comments

--- cryptography.py
+++ cryptography.py
@@ -29,19 +29,30 @@ def createMasterKey(masterKey):
 
 # Encrypt a plaintext message using AES-CBC
 def encrypt(aes_key, plaintext):
+    #The encryption function starts by generating a random IV of 16 bytes
     iv = get_random_bytes(16) 
+    #Then it creates a cipher object using the AES algorithim using the generated IV and the aes_key we derived from the master
     cipher = AES.new(aes_key, AES.MODE_CBC, iv)
+    #the plaintext is padded to the AES block size to compensate for  varying password lengths
     plainText_pad = pad(plaintext.encode(), AES.block_size)
+    #the padded text is then encrypted using the cipher object relying on the encryption function in PyCryptodome
     cipherText = cipher.encrypt(plainText_pad)
+    #The IV is then added to the beggining of the cipher for decryption,
     encrypted = iv + cipherText
+    #then the final encrypted text is encoded in Base64 and returned as a string
     return base64.b64encode(encrypted).decode('utf-8')
 
 # Decrypt an AES-CBC encrypted message
 def decrypt(aes_key, encryptedText):
+    #The decryption function starts by decoding the encrypted text
     encryptedText = base64.b64decode(encryptedText)
+    #the IV is then seperated from the decoded data, the encryption function adds it to the beggining of the password so we seperate the first 16 bytes
     iv = encryptedText[:16]
+    #a cipher object is created same as before using the decoded data minus the IV
     cipherText = encryptedText[16:]
+    #The cipher is decrypted using the cipher object, which returns the padded password
     cipher = AES.new(aes_key, AES.MODE_CBC, iv)
+    #the password is then unpadded, then its decoded, and returned as a string
     decrypted = unpad(cipher.decrypt(cipherText), AES.block_size)
     return decrypted.decode('utf-8')
 
